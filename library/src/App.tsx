f78bc2a74c4d4b68b327a2fd15316d6eec871899
joseph.knoener <joseph.knoener@unasp.edu.br>
2026-02-10 16:56:20 -0300

    Change everything to typescript on the frontend

---
+++ library/src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <>
+    <h1 className="text-yellow-900 text-9xl">Haloo!</h1>
+    <div className="w-100 h-100 bg-yellow-500">Ha!</div>
+    </>    
+  )
+}
+
+export default App
